be7c1b0cf554444a8379e4a203e3a0b5a3d1373b
Michael Bungert <michael.bungert@sap.com>
2023-09-26 20:51:00 +0200

    Initial commit for project setup

---
+++ backend/src/test/java/de/betterboardgames/botcstats/BotCStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.betterboardgames.botcstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotCStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
